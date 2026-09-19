9d3436ed22841c4b4e31aeb2047cee64019628b6
Marc Zaiger <mzaiger@gmail.com>
2026-09-18 19:46:07 -0500

    Update build_nhl_dashboard.py

--- scripts/build_nhl_dashboard.py
+++ scripts/build_nhl_dashboard.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# SCRIPT VERSION: 2026-09-18-mlb-date-aware-odds-match
 """
 NHL Betting Dashboard builder.
 
@@ -454,7 +455,8 @@ def build_day(day, sharp_key, gemini_key=None, previous_odds_by_id=None,
             odds = previous_entry.get("odds") or {}
             frozen_prediction_skip_ids.add(gid_str)
         else:
-            odds = match_odds_for_game(home_team, away_team, odds_rows, team_cache, row_claims)
+            odds = match_odds_for_game(home_team, away_team, odds_rows, team_cache, row_claims,
+                                        target_date=day_str)
             if previous_odds_by_id:
                 odds = carry_forward_odds(odds, previous_odds_by_id.get(event.get("id")))
 
